15066de6029fd10c1b949a5a253cf997a156b954
Anthony Bigotte <bigotte.anthony@gmail.com>
2024-04-08 15:18:57 +0200

    removed suspens boundaries

--- src/app/components/NavBarItem.jsx
+++ src/app/components/NavBarItem.jsx
@@ -1,25 +1,23 @@
 'use client';
+
 import Link from 'next/link';
-import React, { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 
-export default function NavBarItem({ title, param }) {
+export default function NavbarItem({ title, param }) {
   const searchParams = useSearchParams();
   const genre = searchParams.get('genre');
   return (
-    <Suspense>
-      <div>
-        <Link
-          className={`hover:text-amber-600 font-semibold ${
-            genre === param
-              ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
-              : ''
-          }`}
-          href={`/?genre=${param}`}
-        >
-          {title}
-        </Link>
-      </div>
-    </Suspense>
+    <div>
+      <Link
+        className={`hover:text-amber-600 font-semibold ${
+          genre === param
+            ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg'
+            : ''
+        }`}
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link>
+    </div>
   );
 }
